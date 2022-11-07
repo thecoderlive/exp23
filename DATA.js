4442253bb3b907f69e27526f43e83ad47b1f77e0
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-07 14:13:20 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "8808c910-60f0-4353-abf5-05ecb0003e7c",
+    "home_feed": [
+        {
+            "id": "4a4aea9c-79bd-43a0-8645-357caa1d759b",
+            "artist_name": "beautiful",
+            "home_carousel": [
+                {
+                    "id": "a5a69c9e-e6e6-46b5-8777-ef19e6733ec9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "ae4006da-ab7c-4db9-8cb4-2b3455ce832e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "1525d4e5-faf6-46c9-91de-94598267929a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/102.png"
+                },
+                {
+                    "id": "78f994bd-fdb1-4c8c-830a-e24000a12d15",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/71.png"
+                },
+                {
+                    "id": "1d4a58ae-b77e-4292-ad45-1a69490410f8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                }
+            ],
+            "art_caption": "far",
+            "art_description": "Factor southern whose spend wonder. Again commercial try figure message drop mention us. Way buy south couple family."
+        },
+        {
+            "id": "0c7c455f-b5d7-4dff-a527-6f3e67553a47",
+            "artist_name": "good",
+            "home_carousel": [
+                {
+                    "id": "e3669d13-5955-461b-b172-04972dbd12f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/177.png"
+                },
+                {
+                    "id": "f5608188-c9b1-4272-8b24-3254fbf34293",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/172.png"
+                },
+                {
+                    "id": "58e904c3-4039-416b-9e09-72ccc8759350",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "76124ca4-9ed7-4cd2-9637-93176e39ec87",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                },
+                {
+                    "id": "c3f665a6-b169-4fd2-8462-7b86dd98eb16",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                }
+            ],
+            "art_caption": "want",
+            "art_description": "Everything why child thought detail author region. Television article wall writer military student."
+        },
+        {
+            "id": "5d99d0f4-7c2f-4993-9b53-fd9d4e8d719d",
+            "artist_name": "my",
+            "home_carousel": [
+                {
+                    "id": "c0e21261-ad81-466c-b473-07fa0f435bcc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png"
+                },
+                {
+                    "id": "a12e3399-3871-4337-9c32-684f3c8b955e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                },
+                {
+                    "id": "82da6b6e-750a-4bfe-91cd-cf352e7f9cbf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/84.png"
+                },
+                {
+                    "id": "6d3d3d66-1dda-4b1d-b5a7-0e0fe7f48fba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/159.png"
+                },
+                {
+                    "id": "44af34ec-ef12-4258-bf1a-7fddf854163d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/292.png"
+                }
+            ],
+            "art_caption": "little",
+            "art_description": "Share then scene with better style. Coach second direction quite into me. Film senior according short study much. Put hope major pressure official compare."
+        },
+        {
+            "id": "bea9ef1a-65ff-4691-82d4-56e1cb12a3c8",
+            "artist_name": "already",
+            "home_carousel": [
+                {
+                    "id": "fd1510bc-2131-45c3-b17c-781222e3e036",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                },
+                {
+                    "id": "016cd256-3667-4a59-8e9f-c23533b0924b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/319.png"
+                },
+                {
+                    "id": "713a8f77-1465-4ba4-9ca1-4d30c6361bca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                },
+                {
+                    "id": "ac97dd18-e597-4e77-978c-0b419fc37ee6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "bee2ddd7-849b-4593-b042-2bddaff12cd7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                }
+            ],
+            "art_caption": "particular",
+            "art_description": "Direction cover hear reveal true. Teacher off up. Magazine not per save face his."
+        },
+        {
+            "id": "df5713e5-6769-456d-a3fc-0d9b56a37511",
+            "artist_name": "open",
+            "home_carousel": [
+                {
+                    "id": "6294b189-25e4-4f45-862c-db500d665cb9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                },
+                {
+                    "id": "28f45682-ff5f-4598-9f42-2e7282c97401",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "f27ff9a4-13ac-4828-9de4-4cd20529a11b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "43d1484a-b345-4798-9f72-57447d960274",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "a99ff9bb-faec-442b-a4e4-6bebd445dff5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/56.png"
+                }
+            ],
+            "art_caption": "mean",
+            "art_description": "Stand test operation hospital might week attack population. Realize question amount moment laugh."
+        }
+    ]
+}
